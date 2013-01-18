edb7a9d1e8ecb5c75a08d0563ab42a177e595563
Fitorec <chanerec@gmail.com>
2013-01-18 16:36:53 -0600

    Comentando el código fuente

--- numero_de_feminicilios.php
+++ numero_de_feminicilios.php
@@ -25,9 +25,10 @@ class numero_de_feminicilios_widget extends WP_Widget {
 	/**
 	 * Muestra la salida del widget en la página
 	 *
-	 * @param tipo $parametro1 descripción del párametro 1.
-	 * @return tipo descripcion de lo que regresa
-	 * @access publico
+	 * @param array $args los argumentos
+	 * @param array $instance los valores que obtenemos del panel administrativo
+	 * @return void
+	 * @access public
 	 * @link WP_Widget::widget
 	 */
 	function widget($args, $instance) {
@@ -41,7 +42,39 @@ class numero_de_feminicilios_widget extends WP_Widget {
    	 }//end function widget
 
 	/**
-	 * WP_Widget::update -> Se encarga de actualizar el número
+	 * Se encarga de generar el formulario para la parte administrativa.
+	 *
+	 * @param array $instance valores de la instancia
+	 * @return void
+	 * @access public
+	 * @link WP_Widget::form
+	 */
+	function form($instance) {
+		$instance = wp_parse_args (
+			(array) $instance,
+			array(
+				'titulo' => 'Mujeres asesinadas en Oaxaca durante el presente Gobierno:',
+				'numero_de_feminicilios' => 100
+			)
+		);
+		$numero_de_feminicilios = esc_attr($instance['numero_de_feminicilios']);
+		$titulo = esc_attr($instance['titulo']);
+	?>
+	<p>
+		<label for="<?php echo $this->get_field_id('titulo'); ?>">Titulo:</label>
+		<br>
+		<input id="<?php echo $this->get_field_id('titulo'); ?>" name="<?php echo $this->get_field_name('titulo'); ?>" type="text" value="<?php echo $titulo; ?>"/>
+	</p>
+	<p>
+		<label for="<?php echo $this->get_field_id('numero_de_feminicilios'); ?>">Número de feminicilios:</label>
+		<br>
+		<input id="<?php echo $this->get_field_id('numero_de_feminicilios'); ?>" name="<?php echo $this->get_field_name('numero_de_feminicilios'); ?>" type="text" value="<?php echo $numero_de_feminicilios; ?>"/>
+	</p>
+	<?php
+	}//end form function
+
+	/**
+	 * Se encarga de actualizar los datos que recibimos del formulario de la parte administrativa.
 	 *
 	 * @param array $new_instance la nueva instancia donde numero_de_feminicilios tiene que ser un número
 	 * @param array $old_instance la anterior instancia(los datos anteriores)
@@ -56,37 +89,6 @@ class numero_de_feminicilios_widget extends WP_Widget {
 			return $instance;
 	}//end function update
 
-	/**
-	 * Descripción de la función
-	 *
-	 * @param tipo $parametro1 descripción del párametro 1.
-	 * @return tipo descripcion de lo que regresa
-	 * @access publico/privado
-	 * @link WP_Widget::form
-	 */
-	function form($instance) {
-			$instance = wp_parse_args (
-					(array) $instance,
-					array(
-						'titulo' => 'Mujeres asesinadas en Oaxaca durante el presente Gobierno:',
-						'numero_de_feminicilios' => 100
-					)
-				);
-		$numero_de_feminicilios = esc_attr($instance['numero_de_feminicilios']);
-		$titulo = esc_attr($instance['titulo']);
-        ?>
-       <p>
-		<label for="<?php echo $this->get_field_id('titulo'); ?>">Titulo:</label>
-		<br>
-		<input id="<?php echo $this->get_field_id('titulo'); ?>" name="<?php echo $this->get_field_name('titulo'); ?>" type="text" value="<?php echo $titulo; ?>"/>
-	</p>
-	<p>
-		<label for="<?php echo $this->get_field_id('numero_de_feminicilios'); ?>">Número de feminicilios:</label>
-		<br>
-		<input id="<?php echo $this->get_field_id('numero_de_feminicilios'); ?>" name="<?php echo $this->get_field_name('numero_de_feminicilios'); ?>" type="text" value="<?php echo $numero_de_feminicilios; ?>"/>
-	</p>
-	<?php
-    }//end form function
 } // end class numero_de_feminicilios_widget
 
 // registro de Número de feminicilios Widget
